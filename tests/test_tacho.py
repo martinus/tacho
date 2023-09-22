9867ce9b1e9f42ba3932b3a72cd7d4f01a4156f2
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2023-09-22 18:36:44 +0200

    we actually get statistics, wohoo!

--- tests/test_tacho.py
+++ tests/test_tacho.py
@@ -1,10 +1,15 @@
 from importlib.util import spec_from_loader, module_from_spec
 from importlib.machinery import SourceFileLoader
+import inspect
+import os
 
-spec = spec_from_loader("tacho", SourceFileLoader("tacho", "src/tacho"))
+script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) 
+spec = spec_from_loader("tacho", SourceFileLoader("tacho", f"{script_dir}/../src/tacho"))
 tacho = module_from_spec(spec)
 spec.loader.exec_module(tacho)
 
 
-def test_main_succeeds():
-    tacho.main()
+def test_perf():
+    app = tacho.Perf(['ls', '-l'])
+    result = app.run()
+    assert result
